952b3abca18ace4d7f2c40a1164439424d5e15f3
Dani Georgiev <dngeorgiev@abv.bg>
2023-08-08 23:50:56 +0300

    add: `insert` method in `Array`

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ import com.dngeorgiev.arrays.Array;
 public class Main {
     public static void main(String[] args) {
         Array numbers = new Array(3);
+        numbers.insert(1);
+        numbers.insert(10);
+        numbers.insert(30);
+        numbers.insert(420);
+        numbers.insert(69);
         numbers.print();
     }
 }
--- com/dngeorgiev/arrays/Array.java
+++ com/dngeorgiev/arrays/Array.java
@@ -8,6 +8,16 @@ public class Array {
         _items = new int[length];
     }
 
+    public void insert(int item) {
+        if (_items.length == _currentSize) {
+            int[] newItems = new int[_items.length * 2];
+            System.arraycopy(_items, 0, newItems, 0, _items.length);
+            _items = newItems;
+        }
+
+        _items[_currentSize++] = item;
+    }
+
     public void print() {
         for (int i = 0; i < _currentSize; i++) {
             System.out.println(_items[i]);
